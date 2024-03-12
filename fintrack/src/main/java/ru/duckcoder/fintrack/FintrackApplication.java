d1aaa43d2edfd4bbfa4caf5e9cd5ea07b7c2f89b
kudrDaniel <kudr.daniel.vlad.ch@gmail.com>
2024-03-12 10:25:12 +0300

    Initialize spring application

---
+++ fintrack/src/main/java/ru/duckcoder/fintrack/FintrackApplication.java
@@ -0,0 +1,13 @@
+package ru.duckcoder.fintrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FintrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FintrackApplication.class, args);
+	}
+
+}
