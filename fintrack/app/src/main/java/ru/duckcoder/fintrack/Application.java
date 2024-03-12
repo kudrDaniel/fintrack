f5be378fa147a56ba015ae82fb1b383356184532
kudrDaniel <kudr.daniel.vlad.ch@gmail.com>
2024-03-12 13:11:04 +0300

    Reinitialize project

---
+++ fintrack/app/src/main/java/ru/duckcoder/fintrack/Application.java
@@ -0,0 +1,10 @@
+package ru.duckcoder.fintrack;
+
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+public class Application {
+    public static void main(String[] args) {
+        log.info("Entry point reached.");
+    }
+}
